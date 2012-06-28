3737c932ca53323753f2d651500ea43ca9ac6760
Javier Arce <javierarce@gmail.com>
2012-06-28 17:04:37 +0200

    Starts with the orders page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,16 @@
 module ApplicationHelper
-  def snippet(code, options = nil) 
-    comment       = options && options[:comment] 
-    snippet_class = options && options[:snippet_class] 
-    render :partial => "demo/snippet", :locals => { :code => code, :comment => comment, :snippet_class => snippet_class } 
+
+  def checkbox(form, id, name, value)
+    render :partial => "common/checkbox", :locals => { :form => form, :id => id, :name => :name, :value => value }
   end
 
-  def color(code, name) 
-    render :partial => "demo/color", :locals => { :code => code, :name => name } 
+  def snippet(code, options = nil)
+    comment       = options && options[:comment]
+    snippet_class = options && options[:snippet_class]
+    render :partial => "demo/snippet", :locals => { :code => code, :comment => comment, :snippet_class => snippet_class }
+  end
+
+  def color(code, name)
+    render :partial => "demo/color", :locals => { :code => code, :name => name }
   end
 end
