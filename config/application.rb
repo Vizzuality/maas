1db0817dcfe6a056ac6da8a1cf447c08071c7e18
Fernando Espinosa <fer@ferdev.com>
2012-07-31 18:16:21 +0200

    Setups mailer smtp account

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module MaaS
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
     # Activate observers that should always be running.
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+     config.active_record.observers = :order_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -62,11 +62,5 @@ module MaaS
     config.assets.initialize_on_precompile = false
     config.assets.precompile += %w[ active_admin.css active_admin.js ]
 
-    config.action_mailer.smtp_settings = {
-      :address   => 'smtp.mandrillapp.com',
-      :port      => 587,
-      :user_name => ENV['MANDRILL_USERNAME'],
-      :password  => ENV['MANDRILL_PASSWORD']
-    }
   end
 end
