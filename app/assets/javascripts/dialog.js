187528f405bd8f2108ed976a07a266aa294d235f
Javier Arce <javierarce@gmail.com>
2012-07-31 10:48:09 +0200

    Fixes contact form

--- app/assets/javascripts/dialog.js
+++ app/assets/javascripts/dialog.js
@@ -13,11 +13,20 @@ $(function() {
 
 
     initialize: function() {
-      _.bindAll(this, "render", "clean", "error");
+      _.bindAll(this, "render", "clear", "error", "setValue");
 
       this.model = this.options.model;
       this.model.on("change:error", this.error);
+      this.model.on("change:value", this.setValue);
       this.template = this.options.template ? _.template(this.options.template) : cdb.templates.getTemplate(this.model.get("template_name"));
+    },
+
+    setValue: function() {
+      this.$el.find("input,textarea").val(this.model.get("value"));
+    },
+
+    clear: function() {
+      this.model.set("value", "");
     },
 
     error: function() {
@@ -30,7 +39,7 @@ $(function() {
 
     get: function() {
       var value  = this.$el.find("input,textarea").val();
-      this.model.set("v", value);
+      this.model.set("value", value);
     },
 
     render: function() {
@@ -45,19 +54,25 @@ $(function() {
 
     validate: function(attrs) {
 
+      var emailRegexp = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
+
     var error = false;
-      if (!attrs.email) {
+
+      if (!attrs.email || !attrs.email.match(emailRegexp)) {
         window.dialog.email.model.set("error", true);
         error = true;
       }
+
       if (!attrs.name) {
         window.dialog.name.model.set("error", true);
         error = true;
       }
+
       if (!attrs.comment ) {
         window.dialog.comment.model.set("error", true);
         error = true;
       }
+
       if (error) return true;
     }
 
@@ -68,7 +83,7 @@ $(function() {
     initialize: function() {
 
       _.defaults(this.options, this.default_options);
-      _.bindAll(this, 'render', 'keydown', 'ok');
+      _.bindAll(this, 'render', 'keydown', 'hide', 'ok');
 
       $(document).bind('keydown', this.keydown);
 
@@ -106,6 +121,10 @@ $(function() {
 
       this.$el.fadeOut(250);
 
+      $(".backdrop").fadeOut(250, function() {
+        $(this).remove();
+      });
+
     },
 
     open: function() {
@@ -114,11 +133,14 @@ $(function() {
       this.center();
       this.$el.find(".error").removeClass("error");
 
+      $("body").append("<div class='backdrop'></div>");
+      $(".backdrop").fadeIn(250);
+      $(".backdrop").on("click", this.hide);
+
     },
 
     ok: function() {
       var that = this;
-
 
       this.name.model.set("error", false);
       this.email.model.set("error", false);
@@ -128,18 +150,15 @@ $(function() {
       this.email.get();
       this.comment.get();
 
-      this.model.save({ name: this.name.model.get("v"), email: this.email.model.get("v"), comment: this.comment.model.get("v") }, {
+      this.model.save({ name: this.name.model.get("value"), email: this.email.model.get("value"), comment: this.comment.model.get("value") }, {
 
         success: function() {
           that.hide();
-          that.name.$el.find("input").val("");
-          that.email.$el.find("input").val("");
-          that.comment.$el.find("textarea").val("");
 
-          that.name.model.set("v", "");
-          that.email.model.set("v", "");
-          that.comment.model.set("v", "");
-          that.model.clear();
+          that.name.clear();
+          that.email.clear();
+          that.comment.clear();
+
           that.model = new dialogModel();
           that.add_related_model(this.model);
         },
