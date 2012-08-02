6458a3a418c032d27a3fe76f5ba2de0db17226c0
Fernando Espinosa <fer@ferdev.com>
2012-08-02 14:11:24 +0200

    Disables mail sending in production temporally

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ MaaS::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
