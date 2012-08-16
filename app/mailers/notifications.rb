a524b4d6d12a09b6d9bc548487ac72c7da854fc0
Javier Arce <javierarce@gmail.com>
2012-08-16 14:18:02 +0200

    Adds bcc

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,5 +1,5 @@
 class Notifications < ActionMailer::Base
-  default from: "contact@mapmydata.co" #, bcc: "contact@vizzuality.com"
+  default from: "contact@mapmydata.co", bcc: ["contact@vizzuality.com", "javierarce@vizzuality.com"]
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
